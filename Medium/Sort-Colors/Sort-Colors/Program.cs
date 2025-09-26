9048e1f6501eb24c1538815645daa39ffc6fbe43
João Cerri <joaocerri06@gmail.com>
2025-09-25 23:28:42 -0300

    add Sort Colors

---
+++ Medium/Sort-Colors/Sort-Colors/Program.cs
@@ -0,0 +1,47 @@
+﻿public class Solution
+{
+    public void SortColors(int[] nums)
+    {
+        int low = 0;        
+        int mid = 0;       
+        int high = nums.Length - 1;  
+
+        while (mid <= high)
+        {
+            if (nums[mid] == 0)
+            {
+                Swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+            else if (nums[mid] == 1)
+            {
+                mid++;
+            }
+            else
+            { 
+                Swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void Swap(int[] arr, int i, int j)
+    {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 2, 0, 2, 1, 1, 0 };
+        solution.SortColors(nums);
+        Console.WriteLine(string.Join(", ", nums)); 
+    }
+}
+
