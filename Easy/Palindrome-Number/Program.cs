92460ffd964a648fdb63e3ab6e2b772af673dd75
João Cerri <joaocerri06@gmail.com>
2025-10-09 19:58:25 -0300

    add 2 exercises

---
+++ Easy/Palindrome-Number/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Program;
+public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        int original = x;
+        int invertido = 0;
+
+        while (x > 0)
+        {
+            invertido = (invertido * 10) + x % 10;
+            x = x / 10;
+        }
+        if (invertido == original)
+            return true;
+
+        else
+            return false;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int numero = 121;
+        bool resultado = solution.IsPalindrome(numero);
+        Console.WriteLine($"O número {numero} é um palíndromo: {resultado}");
+    }
+}
+
---
+++ Easy/Roman-to-Integer/Program.cs
@@ -0,0 +1,42 @@
+﻿public class Solution
+{
+    public int RomanToInt(string s)
+    {
+        Dictionary<char, int> roman = new Dictionary<char, int>();
+
+        int valor = 0;
+        int valorAnterior = 0;
+
+        roman.Add('I', 1);
+        roman.Add('V', 5);
+        roman.Add('X', 10);
+        roman.Add('L', 50);
+        roman.Add('C', 100);
+        roman.Add('D', 500);
+        roman.Add('M', 1000);
+
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            int valorAtual = roman[s[i]];
+
+            if (valorAtual < valorAnterior)
+            {
+                valor -= valorAtual;
+            }
+            else
+            {
+                valor += valorAtual;
+            }
+
+            valorAnterior = valorAtual;
+        }
+        return valor;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string s = "MCMXCIV";
+        int result = solution.RomanToInt(s);
+        Console.WriteLine(result); 
+    }
+}
