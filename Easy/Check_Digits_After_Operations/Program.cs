95a25841a5542cf6bcf3192cdcb0df7f5766c18a
João Cerri <joaocerri06@gmail.com>
2025-10-23 10:44:15 -0300

    add Check_Digits_After_Operations I

---
+++ Easy/Check_Digits_After_Operations/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool HasSameDigits(string s)
+    {
+
+        while (s.Length > 2)
+        {
+            string newstring = "";
+            for (int i = 0; i < s.Length - 1; i++)
+            {
+                int sum = (s[i] - '0') + (s[i + 1] - '0');
+                newstring += $"{(sum) % 10}";
+            }
+            s = newstring;
+        }
+        return s[0] == s[1];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string s = "1231";
+        bool result = solution.HasSameDigits(s);
+        Console.WriteLine($"Input: {s}, Output: {result}");
+    }
+}
