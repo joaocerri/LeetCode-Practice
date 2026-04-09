07e052fe6839ad4303b3f1d08229f618ae211885
João Cerri <joaocerri18@gmail.com>
2026-04-09 11:18:26 -0300

    feat(leetcode): solve Median of Two Sorted Arrays
    feat: add Median of Two Sorted Arrays
    
    2026-04-09

---
+++ Hard/Median-of-Two-Sorted-Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Solution
+{
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        int[] resultado = nums1.Concat(nums2).OrderBy(x => x).ToArray();
+
+        if (resultado.Length % 2 == 0)
+            return (resultado[(resultado.Length - 1) / 2] + resultado[(resultado.Length) / 2]) / 2.0;
+        else
+            return resultado[resultado.Length / 2];
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums1 = { 1, 3 };
+        int[] nums2 = { 2 };
+        double median = solution.FindMedianSortedArrays(nums1, nums2);
+        Console.WriteLine($"The median is: {median}");
+    }
+}
