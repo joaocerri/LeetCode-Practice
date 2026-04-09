a41aea83d72555518b8fb87ce73a32e55e07f0e1
João Cerri <joaocerri18@gmail.com>
2026-04-09 10:45:31 -0300

    feat(leetcode): solve Robot Return to Origin
    feat: add Robot Return to Origin
    
    2026-04-09

---
+++ Easy/Robot-Return-to-Origin/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Solution
+{
+    public bool JudgeCircle(string moves)
+    {
+        int x = 0, y = 0;
+
+        foreach (char c in moves)
+        {
+            if (c == 'R') x++;
+            else if (c == 'L') x--;
+            else if (c == 'U') y++;
+            else y--;
+        }
+
+        return x == 0 && y == 0;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string moves = "UDLR";
+        bool result = solution.JudgeCircle(moves);
+        Console.WriteLine(result);
+
+        moves = "UDDRRLU";
+        result = solution.JudgeCircle(moves);
+        Console.WriteLine(result);
+    }
+}
