85732afad80764a85276ec603b148fdd4fc4b2ff
João Cerri <joaocerri18@gmail.com>
2026-04-10 22:25:26 -0300

    eat(leetcode): solve Search Insert Position Solved
    2026-04-10

---
+++ Easy/Search-Insert-Position/Program.cs
@@ -0,0 +1,32 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int end = nums.Length - 1;
+        int start = 0;
+        int index = 0;
+
+        while (start <= end)
+        {
+            int middle = start + (end - start) / 2;
+
+            if (nums[middle] == target)
+                return middle;
+
+            if (nums[middle] < target)
+                start = middle + 1;
+
+            else
+                end = middle - 1;
+        }
+        return start;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int[] nums = { 1, 3, 5, 6 };
+        int target = 5;
+        int result = solution.SearchInsert(nums, target);
+        Console.WriteLine(result); 
+    }
+}
