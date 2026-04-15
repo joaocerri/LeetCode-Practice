a092d3c9e210233770292f1b039c99740bd4ad33
João Cerri <joaocerri18@gmail.com>
2026-04-15 19:53:44 -0300

    feat(leetcode): Pow(x, n) solved
    2026-04-15

---
+++ Medium/Pow/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public double MyPow(double x, int n)
+    {
+        long exp = n;
+
+        if (exp < 0)
+        {
+            x = 1 / x;
+            exp = -exp;
+        }
+
+        double result = 1;
+
+        while (exp > 0)
+        {
+            if (exp % 2 == 1)
+            {
+                result *= x;
+            }
+
+            x *= x;
+            exp /= 2;
+        }
+
+        return result;
+    }
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        double x = 5.0;
+        int n = 4;
+        double result = solution.MyPow(x, n);
+        Console.WriteLine($"{x}^{n} = {result}");
+    }
+}
