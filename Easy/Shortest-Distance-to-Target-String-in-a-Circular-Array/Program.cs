9aca7db58ae38b76f0f3a87855b678677b440230
João Cerri <joaocerri18@gmail.com>
2026-04-15 20:18:01 -0300

    feat (leetcode): Shortest Distance to Target String in a Circular Array Solved
    2026-04-15

---
+++ Easy/Shortest-Distance-to-Target-String-in-a-Circular-Array/Program.cs
@@ -0,0 +1,36 @@
+﻿public class Solution
+{
+    public int ClosestTarget(string[] words, string target, int startIndex)
+    {
+        int distance = -1;
+        int minDistance = int.MaxValue;
+        bool found = false;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i] == target)
+            {
+                found = true;
+                distance = i - startIndex;
+                distance = distance < 0 ? -distance : distance;
+                if (distance > words.Length / 2)
+                    distance = words.Length - distance;
+                if (minDistance > distance)
+                    minDistance = distance;
+            }
+        }
+        if (!found) minDistance = -1;
+
+        return minDistance;
+    }
+
+    public static void Main(string[] args)
+    {
+        var solution = new Solution();
+        string[] words = { "hello", "i", "am", "leetcode", "hello" };
+        string target = "hello";
+        int startIndex = 1;
+        int result = solution.ClosestTarget(words, target, startIndex);
+        Console.WriteLine(result); 
+    }
+}
