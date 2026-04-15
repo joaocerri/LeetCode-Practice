dfe646603381e35a4f4592e5be88e3a91102adaf
João Cerri <joaocerri18@gmail.com>
2026-04-15 19:56:51 -0300

    feat(leetcode): Sqrt(x) solved
    2026-04-15

---
+++ Easy/Sqrt/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Solution
+{
+    public int MySqrt(int x)
+    {
+        int start = 0;
+        int end = x;
+
+        if (x < 2)
+            return x;
+
+        while (end >= start)
+        {
+            int mid = start + (end - start) / 2;
+
+            if (mid == x / mid)
+                return mid;
+
+            else if (mid < x / mid)
+                start = mid + 1;
+
+            else
+                end = mid - 1;
+        }
+
+        return start - 1;
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        int x = 64;
+        int result = solution.MySqrt(x);
+        Console.WriteLine(result);
+    }
+}
